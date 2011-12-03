9c03788c451f7838957a94182a083ae75a9680a6
tbranyen <tim@tabdeveloper.com>
2011-12-02 23:28:43 -0500

    force refresh

--- server/lib/server.js
+++ server/lib/server.js
@@ -57,10 +57,7 @@ if (cluster.isMaster) {
   
   // Refresh from the child fork
   process.on("message", function(command) {
-    // Detect if command exists
-    if (command in commands) {
-      commands[command]();
-    }
+    commands["refresh"]();
   });
 
   // Spin up initial forks
