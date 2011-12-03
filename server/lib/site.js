0ed8a9a4b3ae23682c175a6d7c58e6b2171ac334
tbranyen <tim@tabdeveloper.com>
2011-12-02 22:37:08 -0500

    broke out server from the site

--- server/lib/site.js
+++ server/lib/site.js
@@ -1,94 +1,10 @@
-// Require process dependencies
-var os = require("os");
-var readline = require("readline");
-var cluster = require("cluster");
-
-// Var up, bro
-var i, read;
-var forks = [];
-var prefix = "server ~ ";
-
-// Master thread spawns new listeners
-if (cluster.isMaster) {
-  function create() {
-    // Spawn a new worker for each available thread
-    for (i = 0; i < os.cpus().length; i++) {
-      forks.push(cluster.fork());
-    }
-  }
-
-  function destroy() {
-    // Destroy all original forks
-    forks.forEach(function(fork) {
-      fork.send({ cmd: "kill" });
-    });
-
-    forks = [];
-  }
-
-  var command;
-  var commands = {
-    "status": function() {
-      forks.forEach(function(fork) {
-        console.log("Worker", fork.pid, fork.online ? "online" : "offline");
-      });
-    },
-
-    // Destroy all forks and respawn new ones
-    "restart": function() {
-      destroy() && create();
-    },
-
-    // Destroy all forks and kill this master process
-    "stop": function() {
-      destroy();
-
-      process.exit(0);
-    },
-
-    "refresh": function() {
-      forks.forEach(function(fork) {
-        fork.send({ cmd: "refresh" });
-      });
-    }
-  };
-
-  // Spin up initial forks
-  create();
-
-  // Define prompt
-  read = readline.createInterface(process.stdin, process.stdout);
-
-  // Wait for data
-  read.on("line", function(line) {
-    command = line.trim()
-
-    // Detect if command exists
-    if (command in commands) {
-      commands[command]();
-    }
-
-    // Re-Show the prompt
-    read.setPrompt(prefix, prefix.length);
-    read.prompt();
-  })
-  
-  read.on("close", function() {
-    destroy();
-    process.exit(0);
-  });
-
-  // Show prompt
-  read.setPrompt(prefix, prefix.length);
-  read.prompt();
-
-  return;
-}
+console.log("creating");
 
 process.on("message", function(msg) {
   switch (msg.cmd) {
     case "kill":
-      process.exit();
+      console.log("Dying");
+      process.exit(0);
       break;
 
     case "refresh":
