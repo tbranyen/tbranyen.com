1d5d9aaf952b201e7570e6e347706047ea0cab9a
tbranyen <tim@tabdeveloper.com>
2012-02-01 20:40:51 -0500

    updated dependencies to marked and highlight.js

--- server/lib/content/doc.js
+++ server/lib/content/doc.js
@@ -1,8 +1,9 @@
 var fs = require("fs");
+var path = require("path");
 var combyne = require("combyne");
 var storage = require("./temp");
-var hl = require("highlight").Highlight;
-var markdown = require("robotskirt");
+var hl = require("highlight.js");
+var marked = require("marked");
 
 storage.use("../site-content/posts/", "master");
 
@@ -42,30 +43,39 @@ var doc = {
   },
 
   // Take a content file path and render out the content
-  assemble: function(path, callback) {
+  assemble: function(filepath, callback) {
     // Read in the file path
-    storage.file(path + "post.md", "head", function(post) {
+    storage.file(filepath + "post.md", "head", function(post) {
       var contents = post;
       var parts = doc.parse(contents);
       var tmpl = combyne(parts.contents, parts.metadata);
+      var extmap = {
+        ".js": "javascript",
+        ".php": "php",
+        ".lua": "lua"
+      };
 
       // Convert scripts to GitHub flavored markdown
       tmpl.filters.add("render", function(val) {
         var type = val.split(".").pop();
         var codeBlock = "<pre><code>";
-        var lol = fs.readFileSync("../site-content/posts/" + path + "assets/" + val).toString();
+        var lol = fs.readFileSync("../site-content/posts/" + filepath + "assets/" + val).toString();
+        var ext = path.extname(val);
 
-        codeBlock += hl(lol);
+
+        codeBlock += hl.highlight(extmap[ext] || "text", lol).value;
         codeBlock += "</code></pre>";
 
         return codeBlock;
       });
 
+      callback(marked(tmpl.render()));
+
       // Convert the markdown to HTML
-      markdown.toHtml(tmpl.render(), function(html) {
-        var html = html.toString();
-        callback(html);
-      });
+      //markdown.toHtml(tmpl.render(), function(html) {
+      //  var html = html.toString();
+      //  callback(html);
+      //});
     });
   }
 };
