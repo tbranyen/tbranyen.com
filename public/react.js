28c56eaec2a6452f73f681ae7406407bb0730704
Tim Branyen <tim@tabdeveloper.com>
2019-11-04 10:44:24 -0800

    Update react script

--- public/react.js
+++ public/react.js
@@ -1,5 +1,5 @@
 const bind = f => ({ set(t, p, v) { t[p] = v; f(); return !0; } });
 
 export function react(obj, callback) {
-  return new Proxy(obj, bind(() => callback()));
+  return new Proxy(obj, bind(callback));
 }
